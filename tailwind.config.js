04fbf63e467c5ddf0b518a84c25bf736d063dd12
Emre Koc <hi@emrekoc.io>
2021-03-14 15:06:21 +0700

    Add Tailwind and Controls

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [
+      './public/*.html',
+      './src/*',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,32 @@
 const LiveReloadPlugin = require('webpack-livereload-plugin');
-const Path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
 
 const DEV = (process.env.npm_lifecycle_event === 'watch');
 
 module.exports = () => ({
-    mode: DEV ? 'production' : 'development',
+    mode: DEV ? 'development' : 'production',
     devtool: DEV && 'source-map',
     entry: {
-        app: './src/visuals/index.js',
+        controls: './src/controls/index.js',
+        styles: './src/styles.scss',
+        visuals: './src/visuals/index.js',
     },
     module: {
         rules: [
             {
+                test: /\.scss$/i,
+                include: path.resolve(__dirname, 'src'),
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader',
+                ],
+            },
+            {
                 test: /\.(glsl|vs|fs|vert|frag)$/,
-                exclude: /node_modules/,
+                include: path.resolve(__dirname, 'src'),
                 use: [
                     'raw-loader',
                     'glslify-loader',
@@ -23,7 +36,7 @@ module.exports = () => ({
     },
     output: {
         filename: '[name].js',
-        path: Path.resolve(__dirname, 'public/build') + '/',
+        path: path.resolve(__dirname, 'public/build') + '/',
     },
     performance: {
         hints: false,
@@ -32,6 +45,9 @@ module.exports = () => ({
         minimize: !DEV,
     },
     plugins: [
+        new MiniCssExtractPlugin({
+            filename: `[name].css`,
+        }),
         new LiveReloadPlugin(),
     ],
 });
